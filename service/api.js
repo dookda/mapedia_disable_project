543fd28088044937a8df00e707f850e960c20516
dookda <sakda.homhuan@gmail.com>
2022-08-19 18:18:47 +0700

    update

--- service/api.js
+++ service/api.js
@@ -111,7 +111,6 @@ app.post("/api/get_by_amp", async (req, res) => {
     }
 })
 
-
 app.post("/api/get_by_tam", async (req, res) => {
     let { amphoe_code, tambon_code } = req.body
     let connection = await oracledb.getConnection(dbConfig);
@@ -149,4 +148,47 @@ app.post("/api/get_by_tam", async (req, res) => {
     }
 })
 
+
+app.post("/api/get_by_age_type", async (req, res) => {
+    let { province_code, amphoe_code, tambon_code, age, type } = req.body
+    let connection = await oracledb.getConnection(dbConfig);
+    let where;
+    if (!tambon_code) {
+        where = `WHERE CONCAT(mda.PROVINCE_CODE,mda.DISTRICT_CODE) = '${amphoe_code}'`;
+    } else {
+        where = `WHERE CONCAT(mda.PROVINCE_CODE, CONCAT(mda.DISTRICT_CODE, SUBSTR(mda.SUB_DISTRICT, 2))) ='${tambon_code}'`;
+    }
+
+    let sql = `SELECT bp.PROVINCE_CODE, bp.PROVINCE_NAME, 
+            COUNT(bp.PROVINCE_CODE) AS TOTAL,
+            SUM(CASE bd.DEFORM_ID WHEN '0' THEN 1 ELSE 0 END) AS type0, 
+            SUM(CASE bd.DEFORM_ID WHEN '11' THEN 1 ELSE 0 END) AS type12,
+            SUM(CASE bd.DEFORM_ID WHEN '12' THEN 1 ELSE 0 END) AS type13,
+            SUM(CASE bd.DEFORM_ID WHEN '13' THEN 1 ELSE 0 END) AS type14,
+            SUM(CASE bd.DEFORM_ID WHEN '14' THEN 1 ELSE 0 END) AS type15,
+            SUM(CASE bd.DEFORM_ID WHEN '15' THEN 1 ELSE 0 END) AS type16,
+            SUM(CASE bd.DEFORM_ID WHEN '16' THEN 1 ELSE 0 END) AS type17,
+            SUM(CASE bd.DEFORM_ID WHEN '17' THEN 1 ELSE 0 END) AS type18
+        FROM "OPP$_DBA".MN_DES_PERSON mdp 
+        LEFT JOIN "OPP$_DBA".MN_DES_ADDRESS mda ON mdp.MAIMAD_ID = mda.MAIMAD_ID 
+        LEFT JOIN "OPP$_DBA".BS_PROVINCE bp ON mda.PROVINCE_CODE = bp.PROVINCE_CODE 
+        LEFT JOIN "OPP$_DBA".MN_DES_DEFORMED mdd ON mdp.MAIMAD_ID = mdd.MAIMAD_ID 
+        LEFT JOIN "OPP$_DBA".BS_DEFORM bd ON mdd.DEFORM_ID = bd.DEFORM_ID 
+        WHERE bp.PROVINCE_CODE = '40' AND ((SELECT EXTRACT(YEAR FROM SYSDATE) FROM DUAL)-(SUBSTR(mdp.BIRTH_DATE,-4)-543)) < 50
+        GROUP BY bp.PROVINCE_CODE, bp.PROVINCE_NAME`
+    try {
+        const result = await connection.execute(sql, [], { maxRows: 100 });
+        res.status(200).json(result.rows)
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+})
 module.exports = app;
