2c3eac16085dce48252f95695a74d4fd00a09631
dookda <sakda.homhuan@gmail.com>
2022-08-19 07:41:30 +0700

    update api

--- service/api.js
+++ service/api.js
@@ -8,21 +8,25 @@ oracledb.initOracleClient({ libDir: '/Users/sakdahomhuan/instantclient_19_8' });
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 oracledb.autoCommit = true;
 
-app.get("/api/select_region", async (req, res) => {
-    let connection;
-    const sql = `SELECT br.REGION_CODE, br.REGION_NAME_THAI, mdp.SEX_CODE, COUNT(br.REGION_CODE) AS cnt 
-        FROM MN_DES_PERSON mdp 
-        LEFT JOIN MN_DES_ADDRESS mda ON mdp.MAIMAD_ID = mda.MAIMAD_ID 
-        LEFT JOIN BS_PROVINCE bp ON mda.PROVINCE_CODE = bp.PROVINCE_CODE 
-        LEFT JOIN BS_REGION br ON bp.REGION_CODE = br.REGION_CODE 
-        WHERE mda.ADDRESS_CODE = '02'
-        GROUP BY br.REGION_CODE, br.REGION_NAME_THAI, mdp.SEX_CODE
-        ORDER BY br.REGION_NAME_THAI,mdp.SEX_CODE`
+app.post("/api/get_by_region", async (req, res) => {
+    let { address_code } = req.body
+    let connection = await oracledb.getConnection(dbConfig);
+
+    const sql = `SELECT br.REGION_CODE, 
+        br.REGION_NAME_THAI,
+        COUNT(br.REGION_CODE) AS cnt,
+        SUM(CASE mdp.SEX_CODE WHEN 'M' THEN 1 ELSE 0 END) AS M,
+        SUM(CASE mdp.SEX_CODE WHEN 'F' THEN 1 ELSE 0 END) AS F 
+    FROM "OPP$_DBA".MN_DES_PERSON mdp 
+    LEFT JOIN "OPP$_DBA".MN_DES_ADDRESS mda ON mdp.MAIMAD_ID = mda.MAIMAD_ID 
+    LEFT JOIN "OPP$_DBA".BS_PROVINCE bp ON mda.PROVINCE_CODE = bp.PROVINCE_CODE 
+    LEFT JOIN "OPP$_DBA".BS_REGION br ON bp.REGION_CODE = br.REGION_CODE 
+    WHERE mda.ADDRESS_CODE ='${address_code}' GROUP BY br.REGION_CODE, br.REGION_NAME_THAI ORDER BY br.REGION_NAME_THAI`
 
     try {
-        const result = await connection.execute(sql, [], {});
+        const result = await connection.execute(sql, [], { maxRows: 100 });
         res.status(200).json(result.rows)
-    } catch (error) {
+    } catch (err) {
         console.error(err);
     } finally {
         if (connection) {
@@ -35,68 +39,102 @@ app.get("/api/select_region", async (req, res) => {
     }
 })
 
-app.post("/api/getdata", async (req, res) => {
-    const { col, val } = req.body
-    console.log(col, val);
-    let connection;
+app.post("/api/get_by_province", async (req, res) => {
+    let { province_code } = req.body
+    let connection = await oracledb.getConnection(dbConfig);
+    let where;
+    if (province_code == "ALL") {
+        where = `GROUP BY bp.PROVINCE_CODE, bp.PROVINCE_NAME`;
+    } else {
+        where = `WHERE bp.PROVINCE_CODE ='${province_code}' GROUP BY bp.PROVINCE_CODE, bp.PROVINCE_NAME`;
+    }
+
+    const sql = `SELECT bp.PROVINCE_CODE, bp.PROVINCE_NAME, 
+            COUNT(bp.PROVINCE_CODE) AS TOTLE,
+            SUM(CASE mdp.SEX_CODE WHEN 'M' THEN 1 ELSE 0 END) AS M,
+            SUM(CASE mdp.SEX_CODE WHEN 'F' THEN 1 ELSE 0 END) AS F 
+        FROM "OPP$_DBA".MN_DES_PERSON mdp 
+        LEFT JOIN "OPP$_DBA".MN_DES_ADDRESS mda ON mdp.MAIMAD_ID = mda.MAIMAD_ID 
+        LEFT JOIN "OPP$_DBA".BS_PROVINCE bp ON mda.PROVINCE_CODE = bp.PROVINCE_CODE 
+        ${where}`
+
     try {
-        connection = await oracledb.getConnection(dbConfig);
-        console.log('Connection was successful!');
-        // const sql = `SELECT * FROM "OPP$_DBA".MN_DES_ADDRESS`;
-        const sql = `SELECT mdp.*,
-            mdah.ADDRESS_CODE,
-            mdah.SEQ_NO,
-            mdah.ADDRESS,
-            mdah.MOO,
-            mdah.SOI,
-            mdah.STREET,
-            mdah.SUB_DISTRICT,
-            mdah.DISTRICT_CODE,
-            mdah.PROVINCE_CODE,
-            CONCAT(CONCAT(mdah.PROVINCE_CODE,SUBSTR(mdah.DISTRICT_CODE, 2)),mdah.SUB_DISTRICT) AS TAM_CODE, 
-            CONCAT(mdah.PROVINCE_CODE,mdah.DISTRICT_CODE) AS AMP_CODE,
-            mdah.PROVINCE_CODE AS PRO_CODE,
-            mdah.COUNTRY_CODE,
-            mdah.POSTCODE,
-            mdah.TELEPHONE,
-            mdah.FAX,
-            mdah.ADDRESS_NO,
-            mdah.ADDRESS_ID,
-            mdah.HIS_ID,
-            mdd.DEFORM_ID,
-            mdd.DEFORM_LEVEL,
-            mdd.AGE,
-            mdd.DEFORM_CAUSE_ID,
-            mdd.DEF_DESC,
-            mdd.SEQ_NO,
-            mdd.DEFORM_SUB_NAME,
-            mdd.TRANSFER_STATUS,
-            mdd.AUTISTIC_STATUS,
-            mdd.DEFORM_YEAR,
-            mdd.DEFORM_MONTH,
-            mdd.DIAGNOSE_STATUS,
-            mdd.DEF_COMMENT,
-            bd.FROM_TYPE_6_DOCTOR,
-            bd.FORM_TYPE_8_STATUS,
-            bd.FORM_TYPE_8_ALIAS,
-            bd.FORM_COV_STATUS,
-            bd.FORM_COV_ALIAS,
-            bd.DEFORM_NAME,
-            bd.STATUS,
-            bd.CARD_STATUS,
-            bd.CARD_STAFF_STATUS,
-            bd.CARD_DOCTOR_STATUS,
-            bd.DEFORM_ALIAS,
-            bd.FORM_TYPE_6_STATUS,
-            bd.FROM_TYPE_6_STAFF
-        FROM "OPP$_DBA".MN_DES_PERSON mdp
-        LEFT JOIN "OPP$_DBA".MN_DES_ADDRESS_HIS mdah ON mdp.MAIMAD_ID = mdah.MAIMAD_ID 
-        LEFT JOIN "OPP$_DBA".MN_DES_DEFORMED mdd ON mdp.MAIMAD_ID =mdd.MAIMAD_ID 
-        LEFT JOIN "OPP$_DBA".BS_DEFORM bd  ON mdd.DEFORM_ID = bd.DEFORM_ID
-        WHERE ${col}='${val}'`;
-        // console.log(sql);
         const result = await connection.execute(sql, [], { maxRows: 100 });
-        // console.log(result.metaData);
+        res.status(200).json(result.rows)
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+})
+
+app.post("/api/get_by_amp", async (req, res) => {
+    let { province_code, amphoe_code } = req.body
+    let connection = await oracledb.getConnection(dbConfig);
+    let where;
+    if (!amphoe_code) {
+        where = `WHERE mda.PROVINCE_CODE = '${province_code}'`;
+    } else {
+        where = `WHERE CONCAT(mda.PROVINCE_CODE,mda.DISTRICT_CODE)='${amphoe_code}'`;
+    }
+
+    let sql = `SELECT bp.PROVINCE_CODE, bp.PROVINCE_NAME, bd.DISTRICT_CODE, bd.DISTRICT_NAME,
+            COUNT(bp.PROVINCE_CODE) AS TOTLE,
+            SUM(CASE mdp.SEX_CODE WHEN 'M' THEN 1 ELSE 0 END) AS M,
+            SUM(CASE mdp.SEX_CODE WHEN 'F' THEN 1 ELSE 0 END) AS F 
+        FROM "OPP$_DBA".MN_DES_PERSON mdp 
+        LEFT JOIN "OPP$_DBA".MN_DES_ADDRESS mda ON mdp.MAIMAD_ID = mda.MAIMAD_ID 
+        LEFT JOIN "OPP$_DBA".BS_PROVINCE bp ON mda.PROVINCE_CODE = bp.PROVINCE_CODE 
+        LEFT JOIN "OPP$_DBA".BS_DISTRICT bd ON CONCAT(mda.PROVINCE_CODE,mda.DISTRICT_CODE)  = CONCAT(bd.PROVINCE_CODE,bd.DISTRICT_CODE) 
+        ${where}
+        GROUP BY bp.PROVINCE_CODE, bp.PROVINCE_NAME ,bd.DISTRICT_CODE, bd.DISTRICT_NAME`
+
+    try {
+        const result = await connection.execute(sql, [], { maxRows: 100 });
+        res.status(200).json(result.rows)
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+})
+
+
+app.post("/api/get_by_tam", async (req, res) => {
+    let { amphoe_code, tambon_code } = req.body
+    let connection = await oracledb.getConnection(dbConfig);
+    let where;
+    if (!tambon_code) {
+        where = `WHERE CONCAT(mda.PROVINCE_CODE,mda.DISTRICT_CODE) = '${amphoe_code}'`;
+    } else {
+        where = `WHERE CONCAT(mda.PROVINCE_CODE, CONCAT(mda.DISTRICT_CODE, SUBSTR(mda.SUB_DISTRICT, 2))) ='${tambon_code}'`;
+    }
+
+    let sql = `SELECT bp.PROVINCE_CODE, bp.PROVINCE_NAME, bd.DISTRICT_CODE, bd.DISTRICT_NAME, bs.SUBDISTRICT_CODE, bs.SUBDISTRICT_NAME, 
+            COUNT(bp.PROVINCE_CODE) AS TOTLE,
+            SUM(CASE mdp.SEX_CODE WHEN 'M' THEN 1 ELSE 0 END) AS M,
+            SUM(CASE mdp.SEX_CODE WHEN 'F' THEN 1 ELSE 0 END) AS F 
+        FROM "OPP$_DBA".MN_DES_PERSON mdp 
+        LEFT JOIN "OPP$_DBA".MN_DES_ADDRESS mda ON mdp.MAIMAD_ID = mda.MAIMAD_ID 
+        LEFT JOIN "OPP$_DBA".BS_PROVINCE bp ON mda.PROVINCE_CODE = bp.PROVINCE_CODE 
+        LEFT JOIN "OPP$_DBA".BS_DISTRICT bd ON CONCAT(mda.PROVINCE_CODE,mda.DISTRICT_CODE)  = CONCAT(bd.PROVINCE_CODE,bd.DISTRICT_CODE) 
+        LEFT JOIN "OPP$_DBA".BS_SUBDISTRICT bs ON CONCAT(mda.PROVINCE_CODE, CONCAT(mda.DISTRICT_CODE, SUBSTR(mda.SUB_DISTRICT, 2))) = CONCAT(bs.PROVINCE_CODE, CONCAT(bs.DISTRICT_CODE, SUBSTR(bs.SUBDISTRICT_CODE , 2))) 
+        ${where}
+        GROUP BY bp.PROVINCE_CODE, bp.PROVINCE_NAME ,bd.DISTRICT_CODE, bd.DISTRICT_NAME, bs.SUBDISTRICT_CODE, bs.SUBDISTRICT_NAME`
+    try {
+        const result = await connection.execute(sql, [], { maxRows: 100 });
         res.status(200).json(result.rows)
     } catch (err) {
         console.error(err);
