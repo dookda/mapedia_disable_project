97cd70fdf85ebc2538473adeded038750dc35340
dookda <sakda.homhuan@gmail.com>
2022-09-20 21:24:42 +0700

    update

--- www/infocard/app.js
+++ www/infocard/app.js
@@ -1,4 +1,5 @@
-const url = "http://192.168.3.110:3000";
+// const url = "http://192.168.3.110:3000";
+const url = "http://localhost:3000";
 
 $(function () {
   $('#txtDate').datepicker({
@@ -11,22 +12,15 @@ $(function () {
 });
 
 $("#disabilitycard").on('change', function () {
-  if ($("#disabilitycard").val() == "total") {
+  if ($("#disabilitycard").val() == "CRD_NEW") {
     $("#disabilitycard").show();
     $("#reason").hide();
-  }
-  else if ($("#disabilitycard").val() == "firstcard") {
+  } else if ($("#disabilitycard").val() == "CRD_EXP") {
     $("#disabilitycard").show();
     $("#reason").hide();
-  }
-  else if ($("#disabilitycard").val() == "expirecard") {
-    $("#disabilitycard").show();
-    $("#reason").hide();
-  }
-  else if ($("#disabilitycard").val() == "newcard") {
+  } else if ($("#disabilitycard").val() == "CRD_RENEW") {
     $("#disabilitycard").show();
     $("#reason").show();
-  } else {
   }
 })
 
@@ -37,11 +31,16 @@ $("#reason").hide()
 let getData = () => {
   $("#datatable").dataTable().fnDestroy();
   let service_code = document.getElementById("disabilitycard").value
+  let service_code2 = document.getElementById("disabilitycard2").value
   let dtDat = document.getElementById("txtDate").value
   let dtArr = dtDat.split("/")
   let dtTh = `${dtArr[0]}-${dtArr[1]}-${Number(dtArr[2]) + 543}`
 
-  loadTable(service_code, dtTh)
+  if (service_code == "CRD_RENEW") {
+    loadTable(service_code2, dtTh)
+  } else {
+    loadTable(service_code, dtTh)
+  }
 }
 
 let loadTable = (service_code, dtTh) => {
@@ -53,8 +52,10 @@ let loadTable = (service_code, dtTh) => {
       dataSrc: 'data'
     },
     columns: [
+      { data: 'REQUEST_SERVICE_CODE' },
       { data: 'REQUEST_DATE' },
-      { data: 'ISSUE_DATE' },
+      { data: 'CARD_ISSUE_DATE' },
+      { data: 'CARD_EXPIRE_DATE' },
       { data: 'REQUEST_FIRST_NAME' },
       { data: 'REQUEST_LAST_NAME' },
       { data: 'PROVINCE_NAME' },
